ba490503c38d783620025e382ee28fa6d64b3e22
Ansgar Esztermann <aeszter@gwdg.de>
2014-01-15 11:44:49 +0100

    Refactor: Rename Ada.Text_IO.Put_Line

--- src/statistics.adb
+++ src/statistics.adb
@@ -9,31 +9,32 @@ package body Statistics is
    -----------
 
    procedure Print is
+      procedure Put (Item : String) renames Ada.Text_IO.Put_Line;
+
    begin
-      Ada.Text_IO.Put_Line ("Statistics:");
+      Put ("Statistics:");
       if Global_Stats.To_CPU > 0 then
-         Ada.Text_IO.Put_Line (Global_Stats.To_CPU'Img & " jobs migrated to CPU-only queues");
+         Put (Global_Stats.To_CPU'Img & " jobs migrated to CPU-only queues");
       end if;
       if Global_Stats.To_GPU > 0 then
-         Ada.Text_IO.Put_Line (Global_Stats.To_GPU'Img & " jobs migrated to GPU queues");
+         Put (Global_Stats.To_GPU'Img & " jobs migrated to GPU queues");
       end if;
       if No_Slots > 0 then
-         Ada.Text_IO.Put_Line (No_Slots'Img & " jobs not migrated ("
-                               & Global_Stats.No_CPU_Slots'Img & " to CPU/"
-                               & Global_Stats.No_GPU_Slots'Img & " to GPU) "
-                                 & "because there are no free slots");
+         Put (No_Slots'Img & " jobs not migrated ("
+              & Global_Stats.No_CPU_Slots'Img & " to CPU/"
+              & Global_Stats.No_GPU_Slots'Img & " to GPU) "
+                & "because there are no free slots");
       end if;
       if Global_Stats.Range_Reduction > 0 then
-         Ada.Text_IO.Put_Line (Global_Stats.Range_Reduction'Img
-                               & " jobs changed to lower slot numbers");
+         Put (Global_Stats.Range_Reduction'Img
+               & " jobs changed to lower slot numbers");
       end if;
       if Global_Stats.Range_Extension > 0 then
-         Ada.Text_IO.Put_Line (Global_Stats.Range_Extension'Img
+         Put (Global_Stats.Range_Extension'Img
                                & " jobs changed to higher slot numbers");
       end if;
       if Global_Stats.Aimless > 0 then
-         Ada.Text_IO.Put_Line (Global_Stats.Aimless'Img
-                               & " jobs without destination found");
+         Put (Global_Stats.Aimless'Img & " jobs without destination found");
       end if;
    end Print;
 
