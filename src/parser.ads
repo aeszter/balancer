171dfff24d435705288c07797e07451a352d0669
Ansgar Esztermann <aeszter@gwdg.de>
2014-05-16 14:55:50 +0200

    Fix selector string (Bug #2032)
    This is necessary in order to use SGElib v0.9.

--- src/parser.ads
+++ src/parser.ads
@@ -2,7 +2,7 @@ with SGE.Jobs;
 
 package Parser is
    Security_Error : exception;
-   Resource_Selector : constant String := "-F h_rt,eth,ib,ibs,ssd,gpu,mem_total,num_proc,cm,q";
+   Resource_Selector : constant String := "-F h_rt,eth,ib,ibs,ssd,gpu,mem_total,num_proc,cm,q,slots";
 
    procedure Alter_Job (Job                : SGE.Jobs.Job;
       Insecure_Resources : String := "";
